50accff158210743e2cb62a94d26923a8d1980cc
jer <alphastrata@gmail.com>
2025-06-04 12:56:41 +1000

    force build.rs to create the cache_dir, as the call to create_dir_all in the install.rs' un is not working on Windows or Mac

--- crates/cargo-gpu/build.rs
+++ crates/cargo-gpu/build.rs
@@ -1,6 +1,24 @@
 //! cargo-gpu build script.
+fn cache_dir() -> std::path::PathBuf {
+    let dir = directories::BaseDirs::new()
+        .unwrap()
+        .cache_dir()
+        .join("rust-gpu");
+
+    if cfg!(test) {
+        let thread_id = std::thread::current().id();
+        let id = format!("{thread_id:?}").replace('(', "-").replace(')', "");
+        dir.join("tests").join(id)
+    } else {
+        dir
+    }
+}
 
 fn main() {
+    std::fs::create_dir_all(cache_dir()).unwrap();
+
+    assert!(cache_dir().exists());
+
     let git_hash = std::process::Command::new("git")
         .args(["rev-parse", "HEAD"])
         .output()
